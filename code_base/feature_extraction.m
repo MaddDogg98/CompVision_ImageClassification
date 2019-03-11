2a8f28416247cfdc886127c95de301edce8adf74
MaddDogg98 <jdmon1998@gmail.com>
2019-03-11 09:24:56 -0400

    Update feature_extraction.m

--- code_base/feature_extraction.m
+++ code_base/feature_extraction.m
@@ -14,26 +14,5 @@ function feat = feature_extraction(img)
     TB = graythresh(B);
 
     meanColors = [R G B TR TG TB]/255;
-    model = load('model.mat');
-
-    C = model.C;
-    histFeatures = model.histFeatures;
-    testHist = zeros(1,size(C,1));
-    img = rgb2gray(img);
-    points = detectSURFFeatures(img);
-
-    [features,validPoints] = extractFeatures(img, points.selectStrongest(300),'SURFSize',128);
-
-    for k=1:size(features,1)
-    
-        for r=1:size(C,1)
-            a(r) = sqrt(sum((features(k,:) - C(r,:)) .^ 2));
-        end
-        
-        [M,I] = min(a);
-        testHist(I) = testHist(I)+1;
-    end
-
-    feat = testHist/sum(testHist);
-    feat = [feat meanColors];
+   
 end
